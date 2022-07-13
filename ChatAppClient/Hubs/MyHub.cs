827e59afd044c06e5265d531f85922ccc86207ce
MuammerKrc <muammer03karaca@gmail.com>
2022-07-13 23:10:54 +0300

    Add project files.

---
+++ ChatAppClient/Hubs/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppClient.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("GetMessage", message,"mesaj geldi");
+        }
+    }
+}
